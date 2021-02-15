d8f762fa1b90c41251f3a6e0d4572f670f66c8e6
ESmith97C <77297671+ESmith97C@users.noreply.github.com>
2021-02-15 13:36:51 -0600

    Update welcome.js

--- welcome.js
+++ welcome.js
@@ -1,3 +1,4 @@
+function updateTime(){
 var today = new Date();
 
 var hours = today.getHours();
@@ -10,22 +11,20 @@ var seconds = today.getSeconds();
 
 var AmOrPM;
 
-if (today.getHours >= 12){
-    AmOrPM = "PM";
-}
-else{
-    AmOrPM = "AM";
-}
+if (today.getHours() >= 12){AmOrPM = "PM";}
+else{AmOrPM = "AM";}
 
 var finalTime = hours + ":" + minutes + ":" + seconds + AmOrPM;
 
 document.getElementById('timeDisplay').innerHTML = finalTime;
 
 
-if (today.getHours <= 11){document.getElementById('greeting').innerHTML = "Good Morning";}  
-else if (today.getHours >= 12 && today.getHours <= 17){document.getElementById('greeting').innerHTML = "Good Afternoon";}
+if (today.getHours() <= 11){document.getElementById('greeting').innerHTML = "Good Morning";}  
+else if (today.getHours() >= 12 && today.getHours() <= 17){document.getElementById('greeting').innerHTML = "Good Afternoon";}
 else {document.getElementById('greeting').innerHTML = "Good Evening";}
 
 
-if (today.getHours >= 6 && today.getHours <= 18){document.getElementById('sunmoon').src='images/sun.png'}
+if (today.getHours() >= 6 && today.getHours() <= 18){document.getElementById('sunmoon').src='images/sun.png'}
 else {document.getElementById('sunmoon').src='images/moon.png'}
+}
+setInterval(updateTime,1000);
