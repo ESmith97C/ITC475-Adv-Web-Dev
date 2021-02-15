45fb7b158b305e028872757584cf4499f23b2130
ESmith97C <77297671+ESmith97C@users.noreply.github.com>
2021-02-15 13:10:39 -0600

    Create welcome.js

---
+++ welcome.js
@@ -0,0 +1,31 @@
+var today = new Date();
+
+var hours = today.getHours();
+
+hours = ((hours + 11) % 12 + 1);
+
+var minutes = today.getMinutes();
+
+var seconds = today.getSeconds();
+
+var AmOrPM;
+
+if (today.getHours >= 12){
+    AmOrPM = "PM";
+}
+else{
+    AmOrPM = "AM";
+}
+
+var finalTime = hours + ":" + minutes + ":" + seconds + AmOrPM;
+
+document.getElementById('timeDisplay').innerHTML = finalTime;
+
+
+if (today.getHours <= 11){document.getElementById('greeting').innerHTML = "Good Morning";}  
+else if (today.getHours >= 12 && today.getHours <= 17){document.getElementById('greeting').innerHTML = "Good Afternoon";}
+else {document.getElementById('greeting').innerHTML = "Good Evening";}
+
+
+if (today.getHours >= 6 && today.getHours <= 18){document.getElementById('sunmoon').src='sun.png'}
+else {document.getElementById('sunmoon').src='moon.png'}
