a4ed6ee97f4d29976c516439d38f286403c5c1c9
ESmith97C <77297671+ESmith97C@users.noreply.github.com>
2021-02-22 20:56:10 -0600

    Create activitiesselector.js

---
+++ activitiesselector.js
@@ -0,0 +1,32 @@
+
+
+
+function showMaldivesActivites(){
+    document.getElementById("MaldivesActivites").style.visibility = "visible";
+    document.getElementById("MexicoActivites").style.visibility = "display:none";
+    document.getElementById("NewZealandActivites").style.visibility = "display:none";
+    document.getElementById("VeniceActivites").style.visibility = "display:none";
+}
+
+function showMexicoActivites(){
+    document.getElementById("MaldivesActivites").style.visibility = "display:none";
+    document.getElementById("MexicoActivites").style.visibility = "visible";
+    document.getElementById("NewZealandActivites").style.visibility = "display:none";
+    document.getElementById("VeniceActivites").style.visibility = "display:none";
+}
+
+function showNewZealandActivites(){
+    document.getElementById("MaldivesActivites").style.visibility = "display:none";
+    document.getElementById("MexicoActivites").style.visibility = "display:none";
+    document.getElementById("NewZealandActivites").style.visibility = "visible";
+    document.getElementById("VeniceActivites").style.visibility = "display:none";
+}
+
+function showVeniceActivites(){
+    document.getElementById("MaldivesActivites").style.visibility = "display:none";
+    document.getElementById("MexicoActivites").style.visibility = "display:none";
+    document.getElementById("NewZealandActivites").style.visibility = "display:none";
+    document.getElementById("VeniceActivites").style.visibility = "visible";
+}
+
+
