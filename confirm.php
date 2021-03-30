1aed4a2e7c5f8a64713b8e75049a6a8a3b7e4666
ESmith97C <77297671+ESmith97C@users.noreply.github.com>
2021-03-29 20:07:57 -0500

    Create confirm.php

---
+++ confirm.php
@@ -0,0 +1,25 @@
+<html>
+<body>
+    
+Thank you for submitting your travel agent contact request! Here is the information you submitted:
+
+Client Name:<?php echo $_POST["fname" . " " . "lname"]; ?><br>
+
+Client Phone Number:<?php echo $_POST["phone"]; ?><br>
+
+Client Email:<?php echo $_POST["email"]; ?><br>
+
+Number of Adults:<?php echo $_POST["adults"]; ?><br>
+
+Number of Children:<?php echo $_POST["children"]; ?><br>
+
+Destination:<?php echo $_POST["selectedDestination"]; ?><br>
+
+Travel Dates:<?php echo $_POST["date"]; ?><br>
+
+Interested Activities:<?php echo $_POST["selectedActivities"]; ?><br>
+
+An agent will be in touch with you soon!
+
+</body>
+<html>
